0f11cab40d3b22afe2ebdd2a16f7823614806690
dipta <diptachoudhury53@gmail.com>
2023-02-02 13:40:24 +0530

    first day of robfam

---
+++ src/robots.js
@@ -0,0 +1,45 @@
+export const robots=[
+    {
+        id:1,
+        name:"Dipta Choudhury",
+        username:"Dipta9910",
+        email:"diptachoudhury53@gmaill.com"
+    },
+    {
+        id:2,
+        name:"Gajini",
+        username:"gajini9910",
+        email:"gajini53@gmaill.com"
+    },
+    {
+        id:3,
+        name:"salman khan",
+        username:"salman9910",
+        email:"salman53@gmaill.com"
+    },
+    {
+        id:4,
+        name:"Priya shah",
+        username:"priya9910",
+        email:"priya53@gmaill.com"
+    },
+    {
+        id:5,
+        name:"anuska Choudhury",
+        username:"anuska9910",
+        email:"anuska53@gmaill.com"
+    },
+    {
+        id:6,
+        name:"Doron mahajan",
+        username:"Doron9910",
+        email:"doron53@gmaill.com"
+    },
+    {
+        id:7,
+        name:"Dipta Choudhury",
+        username:"Dipta9910",
+        email:"diptachoudhury53@gmaill.com"
+    }
+
+]
